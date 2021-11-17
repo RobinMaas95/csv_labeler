293011bb0af71cb5892600531d4d5f628620ab5a
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2021-11-17 01:21:14 +0100

    Init file for input method tests and add needed Parametrization lib

---
+++ tests/test_input_methods.py
@@ -0,0 +1,10 @@
+ Copyright 2021 Robin Maasjosthusmann. All rights reserved.
+ Use of this source code is governed by a BSD-style
+ license that can be found in the LICENSE file.
+"""
+
+from parametrization import Parametrization
+
+import src.main as main
+
+
